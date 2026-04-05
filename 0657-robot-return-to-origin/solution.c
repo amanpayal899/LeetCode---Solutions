13e68417668f86d04ef6e8be3b01a8fd9e6032b3
aman <amanpayal899@gmail.com>
2026-04-05 17:38:39 +0000

    Sync LeetCode submission Runtime - 3 ms (40.36%), Memory - 8.9 MB (85.84%)

---
+++ 0657-robot-return-to-origin/solution.c
@@ -0,0 +1,18 @@
+bool judgeCircle(char* moves) {
+    int u=0,r=0;
+    for( int i=0; moves[i]!='\0'; i++ )
+    {
+        if( moves[i] == 'U')
+        u++;
+        else if(moves[i] == 'D')
+        u--;
+        else if(moves[i] == 'R')
+        r++;
+        else if(moves[i] == 'L')
+        r--;
+    }
+    if(u==0 && r==0)
+    return true;
+    return false;
+}
+
