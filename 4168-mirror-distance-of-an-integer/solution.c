1ccaf89dbb8455911faa97209426bf667f375e60
aman <amanpayal899@gmail.com>
2026-04-18 14:32:50 +0000

    Sync LeetCode submission Runtime - 4 ms (1.00%), Memory - 9.1 MB (94.02%)

---
+++ 4168-mirror-distance-of-an-integer/solution.c
@@ -0,0 +1,11 @@
+int mirrorDistance(int n) {
+    int rev = 0;
+    int temp = n;
+    while(temp>0){
+        rev = rev*10 + temp%10;
+        temp = temp/10;
+    }
+    if( rev > n )
+        return rev-n;
+    return n-rev;
+}
