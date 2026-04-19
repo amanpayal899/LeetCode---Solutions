a45b9f029ec308589a4f8b99525b550be34c2644
aman <amanpayal899@gmail.com>
2026-04-19 16:19:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.9 MB (88.24%)

--- 1984-maximum-distance-between-a-pair-of-values/solution.c
+++ 1984-maximum-distance-between-a-pair-of-values/solution.c
@@ -1,24 +1,15 @@
 int maxDistance(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int max_dis=0;
-    for( int i=0; i<nums1Size; i++ )
+    int max_distance = 0; 
+    int i=0, j=0;
+    while( i<nums1Size && j<nums2Size )
     {
-        int start=i, end=nums2Size-1;
-        while(start<=end)
+        if( nums1[i] <= nums2[j] )
         {
-            int mid = ((end-start)/2)+start;
-            if( nums2[mid]<nums1[i] )
-            {
-                end=mid-1;
-            }
-            else if( nums2[mid]>=nums1[i])
-            {
-               if(max_dis<mid-i) 
-               {
-                    max_dis=mid-i;
-               }
-               start=mid+1;
-            }
+            if( j-i > max_distance && j>=i )
+            max_distance = j-i;
+            j++;
         }
+        else i++;
     }
-    return max_dis;
+    return max_distance;
 }
