46c57eb4586205ee5b7ac44fba882ce98abcdb4a
aman <amanpayal899@gmail.com>
2026-07-07 07:09:15 +0000

    Sync LeetCode submission Runtime - 59 ms (59.74%), Memory - 22.3 MB (7.12%)

--- Solutions/1046-max-consecutive-ones-iii/solution.py
+++ Solutions/1046-max-consecutive-ones-iii/solution.py
@@ -1,21 +1,18 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        l, r = 0, 0
-        zeroes = 0
+        left, right = 0, 0
         maxi = 0
-        my_queue = deque()
-        while r<n:
-            while  r < n and (nums[r] == 1 or zeroes < k):
-                if nums[r] == 0:
-                    my_queue.append(r)
-                    zeroes += 1
-                r += 1
-            maxi = max(maxi, r-l)
-            if my_queue:
-                l = my_queue.popleft() + 1
-                zeroes = k-1
-            else:
-                r += 1
-                l = r
+        zeroes = 0
+        n = len(nums)
+
+        while right < n:
+            if nums[right] == 0:
+                zeroes += 1
+            if zeroes > k:
+                if nums[left] == 0:
+                    zeroes -= 1
+                left += 1
+            maxi = max(maxi, right-left+1)
+            right += 1
+
         return maxi
