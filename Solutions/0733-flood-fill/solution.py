9e69808026b462876327ab4521d21b6e0e94a069
aman <amanpayal899@gmail.com>
2026-07-30 16:02:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (64.79%)

---
+++ Solutions/0733-flood-fill/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if color == image[sr][sc]:
+            return image
+        r = len(image)
+        c = len(image[0])
+        startColor = image[sr][sc]
+        image[sr][sc] = color
+        queue = deque()
+        queue.append((sr, sc))
+
+        while queue:
+            size = len(queue)
+            while size>0:
+                i, j = queue.popleft()
+                if ( i>0 and image[i-1][j]==startColor ):
+                    queue.append((i-1, j))
+                    image[i-1][j] = color
+                if ( i<r-1 and image[i+1][j]==startColor ):
+                    queue.append((i+1, j))
+                    image[i+1][j] = color
+                if ( j>0 and image[i][j-1]==startColor ):
+                    queue.append((i, j-1))
+                    image[i][j-1] = color
+                if ( j<c-1 and image[i][j+1]==startColor ):
+                    queue.append((i, j+1))
+                    image[i][j+1] = color
+                size -= 1
+            
+        return image
+                
+
