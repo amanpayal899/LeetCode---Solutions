5c7fc4e4c33aac23cc5c259b18988581ed29108b
aman <amanpayal899@gmail.com>
2026-08-13 18:05:07 +0000

    Sync LeetCode submission Runtime - 155 ms (24.26%), Memory - 23.5 MB (7.98%)

---
+++ Solutions/0542-01-matrix/solution.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        rows = len(mat)
+        cols = len(mat[0])
+        queue = deque()
+        result = copy.deepcopy(mat)
+        visited = [ [0]*cols for _ in range(rows)]
+
+        def dfs(rows, cols, queue, result, visited):
+            for  r in range(rows):
+                for c in range(cols):
+                    if result[r][c] == 0:
+                        queue.append( (r, c, 0))
+                        visited[r][c] = 1
+
+            while len(queue)!=0:
+                r, c, dis = queue.popleft()
+                result[r][c] = dis
+                for i, j in [ [-1,0], [0, 1], [1,0], [0, -1] ]:
+                    nr, nc = r+i, c+j
+                    if nr<0 or nr>=rows or nc<0 or nc>=cols or visited[nr][nc] :
+                        continue
+                    queue.append( (nr, nc, dis+1) )
+                    visited[nr][nc]=1
+
+
+        dfs(rows, cols, queue, result, visited)
+        return result
+        
