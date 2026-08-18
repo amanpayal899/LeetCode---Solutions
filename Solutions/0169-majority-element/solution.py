c355bc3115db4c88d6632c932d29f1403b0bc6e9
aman <amanpayal899@gmail.com>
2026-08-18 20:31:41 +0000

    Sync LeetCode submission Runtime - 15 ms (18.66%), Memory - 21.4 MB (18.50%)

--- Solutions/0169-majority-element/solution.py
+++ Solutions/0169-majority-element/solution.py
@@ -1,4 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        d = {}
+        n = len(nums)
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+            if d[i] > n//2:
+                return i
+        return 0
