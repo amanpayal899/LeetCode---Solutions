b55e326bd2be5ad967fdfb9efa1fbba263821103
aman <amanpayal899@gmail.com>
2026-08-28 20:14:44 +0000

    Sync LeetCode submission Runtime - 7 ms (33.20%), Memory - 19.3 MB (74.15%)

---
+++ Solutions/0415-add-strings/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        carry = 0
+        result = ""
+        ptr1, ptr2 = len(num1)-1, len(num2)-1
+        while ptr1>=0  and ptr2>=0:
+            temp = int(num1[ptr1]) + int(num2[ptr2]) + carry
+            if temp > 9:
+                carry = temp//10
+                temp = temp%10
+            else:
+                carry = 0 
+            result = str(temp) + result
+            ptr1-=1
+            ptr2-=1
+        while ptr1>=0:
+            temp = int(num1[ptr1]) + carry
+            result = str(temp%10) +  result
+            carry = temp//10
+            ptr1-=1
+        while ptr2>=0:
+            temp = int(num2[ptr2]) + carry
+            result = str(temp%10) +  result
+            carry = temp//10
+            ptr2-=1
+        if carry:
+            result = str(carry) + result
+        return result
+        
+
+
